0db33bc31e55fc3c1200cd6c1d72f1e6dcfa0866
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-05-15 21:32:05 +0900

    feat: Register items on each loader

--- neoforge/src/main/java/com/github/iunius118/laserbladetools/registry/NeoForgeModRegistries.java
+++ neoforge/src/main/java/com/github/iunius118/laserbladetools/registry/NeoForgeModRegistries.java
@@ -26,6 +26,14 @@ public class NeoForgeModRegistries {
     private static void registerItems(IEventBus modEventBus) {
         var items = DeferredRegister.createItems(Constants.MOD_ID);
 
+        items.register(Constants.Items.LB_CORE.getPath(), () -> ModItems.LB_CORE);
+        items.register(Constants.Items.LB_SWORD.getPath(), () -> ModItems.LB_SWORD);
+        items.register(Constants.Items.LB_SHOVEL.getPath(), () -> ModItems.LB_SHOVEL);
+        items.register(Constants.Items.LB_PICKAXE.getPath(), () -> ModItems.LB_PICKAXE);
+        items.register(Constants.Items.LB_AXE.getPath(), () -> ModItems.LB_AXE);
+        items.register(Constants.Items.LB_HOE.getPath(), () -> ModItems.LB_HOE);
+        items.register(Constants.Items.LB_SPEAR.getPath(), () -> ModItems.LB_SPEAR);
+
         items.register(modEventBus);
     }
 
