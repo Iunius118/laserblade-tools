38c9f97c25e9c0d8fe0d93d751dd133bf744d81d
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-05-31 23:54:38 +0900

    fix: Temporary render type fix

--- common/src/main/java/com/github/iunius118/laserbladetools/client/ModRenderTypes.java
+++ common/src/main/java/com/github/iunius118/laserbladetools/client/ModRenderTypes.java
@@ -1,60 +1,43 @@
 package com.github.iunius118.laserbladetools.client;
 
 import com.github.iunius118.laserbladetools.Constants;
-import com.github.iunius118.laserbladetools.mixin.client.RenderTypeInvoker;
-import com.mojang.blaze3d.pipeline.BlendFunction;
-import com.mojang.blaze3d.pipeline.ColorTargetState;
-import com.mojang.blaze3d.pipeline.DepthStencilState;
-import com.mojang.blaze3d.pipeline.RenderPipeline;
-import com.mojang.blaze3d.shaders.UniformType;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.renderer.rendertype.OutputTarget;
-import net.minecraft.client.renderer.rendertype.RenderSetup;
-import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.resources.Identifier;
-import net.minecraft.util.Util;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.BiFunction;
 
-public class ModRenderTypes {
-	// Unlit render pipeline using entity shader
-	public static final RenderPipeline UNLIT_TRANSLUCENT_PIPELINE = RenderPipeline.builder()
-			.withLocation("pipeline/lb_unlit_translucent")
-			.withUniform("DynamicTransforms", UniformType.UNIFORM_BUFFER)
-			.withUniform("Projection", UniformType.UNIFORM_BUFFER)
-			.withUniform("Fog", UniformType.UNIFORM_BUFFER)
-			.withUniform("Lighting", UniformType.UNIFORM_BUFFER)
-			.withVertexShader("core/entity")
-			.withFragmentShader("core/entity")
-			.withSampler("Sampler0")
-			.withVertexFormat(DefaultVertexFormat.ENTITY, VertexFormat.Mode.QUADS)
-			.withDepthStencilState(DepthStencilState.DEFAULT)
-			.withShaderDefine("EMISSIVE")
-			.withShaderDefine("NO_OVERLAY")
-			.withShaderDefine("NO_CARDINAL_LIGHTING")
-			.withShaderDefine("ALPHA_CUTOUT", 0.1F)
-			.withColorTargetState(new ColorTargetState(BlendFunction.TRANSLUCENT))
-			.build();
+public class ModRenderTypes extends RenderType {
+    private static final RenderStateShard.ShaderStateShard LASER_BLADE_UNLIT_SHADER_STATE = getUnlitShader();
 
-	private static final BiFunction<String, Identifier, RenderType> UNLIT_TRANSLUCENT = Util.memoize(
-			(name, texture) -> {
-				RenderSetup renderSetup = RenderSetup.builder(UNLIT_TRANSLUCENT_PIPELINE)
-						.withTexture("Sampler0", texture)
-						.setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
-						.sortOnUpload()
-						.setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
-						.createRenderSetup();
-				return RenderTypeInvoker.invokeCreate(name, renderSetup);
-			}
-	);
+    private static final BiFunction<String, ResourceLocation, RenderType> UNLIT_TRANSLUCENT =
+            (name, texture) -> {
+                RenderType.CompositeState state = RenderType.CompositeState.builder()
+                        .setShaderState(LASER_BLADE_UNLIT_SHADER_STATE)
+                        .setTextureState(new TextureStateShard(texture, false, false))
+                        .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                        .createCompositeState(true);
+                return create(name, DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 1536, false, true, state);
+    };
 
-	public static RenderType unlit(Identifier texture) {
-		return UNLIT_TRANSLUCENT.apply(Constants.MOD_ID + ":lb_unlit", texture);
+    public ModRenderTypes(String name, VertexFormat format, VertexFormat.Mode mode, int bufferSize,
+                          boolean affectsCrumbling, boolean sortOnUpload, Runnable setupState, Runnable clearState) {
+        super(name, format, mode, bufferSize, affectsCrumbling, sortOnUpload, setupState, clearState);
+    }
+
+    private static RenderStateShard.ShaderStateShard getUnlitShader() {
+        // Temporarily set beacon beam shader from vanilla shaders
+        return RENDERTYPE_BEACON_BEAM_SHADER;
+    }
+
+    public static RenderType unlit(ResourceLocation texture) {
+		return UNLIT_TRANSLUCENT.apply(Constants.RenderTypes.LASER_BLADE_UNLIT, texture);
 	}
 
 	public static RenderType unlitItem() {
-		return unlit(TextureAtlas.LOCATION_ITEMS);
+		return unlit(TextureAtlas.LOCATION_BLOCKS);
 	}
 }
