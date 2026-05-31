18a1c7493b8b3f146b7dca475b6ff0c9a5c3a5ab
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-06-01 08:50:45 +0900

    fix: Fix mod render type for 1.21.1 compatibility

--- common/src/main/java/com/github/iunius118/laserbladetools/client/ModRenderTypes.java
+++ common/src/main/java/com/github/iunius118/laserbladetools/client/ModRenderTypes.java
@@ -3,7 +3,6 @@ package com.github.iunius118.laserbladetools.client;
 import com.github.iunius118.laserbladetools.Constants;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.ResourceLocation;
@@ -11,16 +10,16 @@ import net.minecraft.resources.ResourceLocation;
 import java.util.function.BiFunction;
 
 public class ModRenderTypes extends RenderType {
-    private static final RenderStateShard.ShaderStateShard LASER_BLADE_UNLIT_SHADER_STATE = getUnlitShader();
+    private static final ShaderStateShard LASER_BLADE_UNLIT_SHADER_STATE = getUnlitShader();
 
     private static final BiFunction<String, ResourceLocation, RenderType> UNLIT_TRANSLUCENT =
             (name, texture) -> {
-                RenderType.CompositeState state = RenderType.CompositeState.builder()
+                CompositeState state = RenderType.CompositeState.builder()
                         .setShaderState(LASER_BLADE_UNLIT_SHADER_STATE)
                         .setTextureState(new TextureStateShard(texture, false, false))
                         .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
                         .createCompositeState(true);
-                return create(name, DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 1536, false, true, state);
+                return create(name, DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 1536, false, false, state);
     };
 
     public ModRenderTypes(String name, VertexFormat format, VertexFormat.Mode mode, int bufferSize,
@@ -28,9 +27,9 @@ public class ModRenderTypes extends RenderType {
         super(name, format, mode, bufferSize, affectsCrumbling, sortOnUpload, setupState, clearState);
     }
 
-    private static RenderStateShard.ShaderStateShard getUnlitShader() {
-        // Temporarily set beacon beam shader from vanilla shaders
-        return RENDERTYPE_BEACON_BEAM_SHADER;
+    private static ShaderStateShard getUnlitShader() {
+        // Temporarily set position color texture (lightmap) shader from vanilla shaders
+        return POSITION_COLOR_TEX_LIGHTMAP_SHADER;
     }
 
     public static RenderType unlit(ResourceLocation texture) {
