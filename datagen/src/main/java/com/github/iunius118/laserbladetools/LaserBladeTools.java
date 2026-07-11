e30bcb0d30f2f8f16e29095ccd84eae69a19dc99
iunius118 <18502177+Iunius118@users.noreply.github.com>
2026-07-11 23:05:48 +0900

    fix: Move common game object registrations to `common` module - Add ModObjectRegistry interface - Add methods to platform helpers - Add CreativeModeTab.Output to dev/accesstransformer.cfg

--- datagen/src/main/java/com/github/iunius118/laserbladetools/LaserBladeTools.java
+++ datagen/src/main/java/com/github/iunius118/laserbladetools/LaserBladeTools.java
@@ -1,7 +1,6 @@
 package com.github.iunius118.laserbladetools;
 
 import com.github.iunius118.laserbladetools.data.*;
-import com.github.iunius118.laserbladetools.registry.DataGenModRegistries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -9,14 +8,16 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @Mod(Constants.MOD_ID)
 public class LaserBladeTools {
+    public static IEventBus modEventBus;
 
     public LaserBladeTools(IEventBus modEventBus, ModContainer modContainer) {
+        LaserBladeTools.modEventBus = modEventBus;
+
         // Use NeoForge to bootstrap the Common mod.
         //Constants.LOG.info("Hello DataGen world!");
         CommonClass.init();
 
         // Register mod event listeners
-        DataGenModRegistries.registerGameObjects(modEventBus);
         modEventBus.addListener(this::gatherData);
     }
 
--- forge/src/main/java/com/github/iunius118/laserbladetools/platform/ForgePlatformHelper.java
+++ forge/src/main/java/com/github/iunius118/laserbladetools/platform/ForgePlatformHelper.java
@@ -3,9 +3,14 @@ package com.github.iunius118.laserbladetools.platform;
 import com.github.iunius118.laserbladetools.network.ColorSelectionPayload;
 import com.github.iunius118.laserbladetools.network.ModChannels;
 import com.github.iunius118.laserbladetools.platform.services.IPlatformHelper;
+import com.github.iunius118.laserbladetools.registry.ForgeModObjectRegistry;
+import com.github.iunius118.laserbladetools.registry.ModObjectRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.registries.DeferredRegister;
 
 public class ForgePlatformHelper implements IPlatformHelper {
 
@@ -27,5 +32,15 @@ public class ForgePlatformHelper implements IPlatformHelper {
     @Override
     public void sendColorSelectionPayloadToServer(ColorSelectionPayload payload) {
         ModChannels.PAYLOAD_CHANNEL.send(payload, PacketDistributor.SERVER.noArg());
+    }
+
+    @Override
+    public <V, T extends V> ModObjectRegistry<V, T> createModObjectRegistry(Registry<V> registry, String namespace) {
+        return new ForgeModObjectRegistry<>(DeferredRegister.create(registry.key(), namespace), namespace);
+    }
+
+    @Override
+    public CreativeModeTab.Builder createCreativeModeTabBuilder() {
+        return CreativeModeTab.builder();
     }
 }
